47f8015e12b6907423c0d361e5932dec9d940f88
less6498-blip <less6498@gmail.com>
2026-04-21 18:27:11 -0500

    fix: https assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,20 +8,19 @@ use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function boot(): void
-    {
-        // Solo forzar HTTPS en producción (Railway)
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-            
-            Request::setTrustedProxies(
-                ['*'],
-                Request::HEADER_X_FORWARDED_FOR |
-                Request::HEADER_X_FORWARDED_HOST |
-                Request::HEADER_X_FORWARDED_PORT |
-                Request::HEADER_X_FORWARDED_PROTO |
-                Request::HEADER_X_FORWARDED_AWS_ELB
-            );
-        }
+   public function boot(): void
+{
+    if (app()->environment('production')) {
+        URL::forceScheme('https');
+        
+        Request::setTrustedProxies(
+            ['*'],
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     }
+}
 }
