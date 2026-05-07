962719b0daae0a37b344f7d6b906d960b6e2f3f7
less6498-blip <less6498@gmail.com>
2026-05-07 08:45:52 -0500

    fix: admin seeder firstOrCreate

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -9,7 +9,7 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        Admin::create([
+        Admin::firstOrCreate([
             'nombre'   => 'Administrador',
             'email'    => 'admin@elenex.com',
             'password' => Hash::make('Elenex2024$'),
